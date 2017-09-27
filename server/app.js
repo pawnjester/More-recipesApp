09f31ad43b2f59b9e5e8756a713835ad9ffca694
Okonji emmanuel <ebukeokonji@gmail.com>
2017-09-27 23:00:39 +0100

    Add .babelrc

--- server/app.js
+++ server/app.js
@@ -2,4 +2,16 @@ import express from 'express';
 import logger from 'morgan';
 import bodyParser from 'body-parser';
 
+const app = express();
+const port = process.env.PORT || 8000;
 
+app.use(logger('dev'));
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+app.listen(port, () => {
+});
+
+console.log(`Application has started on  ${port}`);
+
+export default app;
