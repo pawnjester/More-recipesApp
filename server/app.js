3caf258e81448c62b947c7809adddb2635f00c70
Okonji emmanuel <ebukeokonji@gmail.com>
2017-09-28 00:31:55 +0100

    Add routes for modifying and deleting a recipe

--- server/app.js
+++ server/app.js
@@ -34,10 +34,14 @@ app.get('/api/recipes', function(req, res) {
 });
 
 app.post('/api/recipes', function(req, res) {
+  const id = req.body.id;
   const name = req.body.name;
   const ingredients = req.body.ingredients;
   const method = req.body.method;
 
+  if(!id) {
+    return res.status(400).send({statusCode:400, message: "Put in an id"});
+  }
   if(!name) {
     return res.status(400).send({statusCode:400, message: "Put in the name of the recipe"});
   }
@@ -49,6 +53,28 @@ app.post('/api/recipes', function(req, res) {
   }
   global.recipes.push(req.body);
   return res.status(201).send({status: 201, message: "A New Recipe added", recipes:global.recipes});
+});
+
+app.put('/api/recipes/:recipeId', function(req, res) {
+  for(let i= 0; i < global.recipes.length; i++) {
+    if(global.recipes[1].id === parseInt(req.params.recipeId, 10)) {
+      global.recipes[1].name = req.body.name;
+      global.recipes[1].ingredients = req.body.ingredients;
+      global.recipes[1].method = req.body.method;      
+      return res.status(200).send({message: "Recipe has been modified"})
+    }
+  }
+  return res.status(404).send({message: "User not found"})
+})
+
+app.delete('/api/recipes/:recipeId', function(req, res) {
+  for(let i= 0; i < global.recipes.length; i++) {
+    if(global.recipes[1].id === parseInt(req.params.recipeId, 10)) {
+      global.recipes.splice(1,1)    
+      return res.status(200).send({message: "Recipe has been removed"})
+    }
+  }
+  return res.status(404).send({message: "User not found"})
 })
 
 
