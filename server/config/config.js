96035c33462567add40416438a19eeffcf8b789f
Okonji emmanuel <ebukeokonji@gmail.com>
2017-09-30 13:05:25 +0100

    Setup database

---
+++ server/config/config.js
@@ -0,0 +1,16 @@
+{
+  "development": {
+    "username": "postgres",
+    "password": "chartherpharmacy",
+    "database": "recipes-dev",
+    "host": "127.0.0.1",
+    "dialect": "postgres",      
+  },
+  "test": {
+    "username": "postgres",
+    "password": "chartherpharmacy",
+    "database": "recipes-dev",
+    "host": "127.0.0.1",
+    "dialect": "postgres"
+  }
+}
