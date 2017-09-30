44569a86923846573b008bf97d508088776f075b
Okonji emmanuel <ebukeokonji@gmail.com>
2017-09-30 09:52:12 +0100

    restructure add-reviews

--- server/controllers/reviews.js
+++ server/controllers/reviews.js
@@ -10,8 +10,8 @@ class Reviews {
     }
     for (let i = 0; i < db.recipes.length; i++) {
     if (parseInt(db.recipes[i].id, 10) === parseInt(req.params.recipeId, 10)) {
-      // db.recipes[i].review = review;
-      db.recipes[i].push(req.body);      
+      db.recipes[i].review = review;
+      // db.recipes[i].push(req.body);      
       return res.status(200).send({ message: "Review has been added" });
     }
   }
