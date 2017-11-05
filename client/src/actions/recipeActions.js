801314a148de0b20bce54bae2e499020794f657e
Okonji emmanuel <ebukeokonji@gmail.com>
2017-11-05 22:58:18 +0100

    test a react project

--- client/src/actions/recipeActions.js
+++ client/src/actions/recipeActions.js
@@ -1,9 +1,18 @@
 /* eslint-disable */
 
-import axios from 'axios';
+import { RECIPE } from './types'
 
 export function createRecipe(recipe) {
-  return dispatch => {
-    return axios.post('/api/v1/recipes', recipe)
+  // return dispatch => {
+  //   return axios.post('/api/v1/recipes', recipe)
+  // }
+
+  const action =  {
+    //unique identifier
+    type: 'RECIPE',
+    //Payload
+    recipe
+        
   }
+  return action;
 }
--- client/src/rootReducer.js
+++ client/src/rootReducer.js
@@ -1,9 +1,11 @@
-import  {combineReducers} from 'redux';
+import { combineReducers } from 'redux';
 import flashMessages from './reducers/flashMessages';
 import auth from './reducers/auth';
+import red_recipe from './reducers/red_recipe';
 
 
 export default combineReducers({
   flashMessages,
-  auth
+  auth,
+  red_recipe
 });
