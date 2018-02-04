243631ccf9410d1f8a120cacf11863d2f61d8741
okonji emmanuel <ebukeokonji@gmail.com>
2018-02-04 17:13:08 +0100

    fix-travis

--- server/helper/reviewMailer.js
+++ server/helper/reviewMailer.js
@@ -76,7 +76,7 @@ const mailer = (name, reviewer, email, review, recipeName, res) => {
     if (error) {
       return res.status(500).json({ error: 'Unable to send mail' });
     }
-    return res.json({ message: 'Recovery link sent to your mail' });
+    res.json({ message: 'Recovery link sent to your mail' });
   });
 };
 
