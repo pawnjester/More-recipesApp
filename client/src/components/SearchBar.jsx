ff6d0207112509a80c777ce4e04c3a8bfa9c011c
Okonji Emmanuel <ebukeokonji@gmail.com>
2018-02-04 14:26:22 +0100

    rename searchBar.jsx to SearchBar.jsx

--- client/src/components/SearchBar.jsx
+++ client/src/components/SearchBar.jsx
@@ -2,7 +2,7 @@ import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import PropTypes from 'prop-types';
 import searchRecipe from '../actions/searchRecipe';
-/**
+/*
  *
  * @class searchBar
  * @extends {Component}
